ebc6e60aabbf66f2f08b3117a8e9d2481582fca9
Golam Kibria Ezaz <golamkibria3435@gmail.com>
2022-08-05 21:21:30 +0600

    js

---
+++ 14. Function/1. basic.js
@@ -0,0 +1,9 @@
+function myFunction(a, b){
+    return a * b
+}
+let x = myFunction(2, 3) 
+console.log(x) 
+/*
+Function parameters are listed inside the parentheses () in the function definition.
+Function arguments are the values received by the function when it is invoked.
+*/ 
---
+++ 14. Function/2. faren-cel.js
@@ -0,0 +1,5 @@
+function toCel(faren){
+    return (5/9) * (faren - 92); 
+}
+let x = toCel(80); 
+console.log("The celcious temp is: ", x) 
